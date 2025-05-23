303f2e16e8988b512ea248621f901af537cabd05
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-23 10:50:08 +0530

    learn about the path speciifc  route

--- Lec1/index.js
+++ Lec1/index.js
@@ -6,36 +6,48 @@ const port = 3000
 
 
 
-//middleware -logging , auth , validation
+/**
+ * 
+ * 
+             //middleware -logging , auth , validation
 
-//first middleware
-//creation of middlewares
- const  loggingMiddleware = function(req, res, next){
-     console.log("first middleware is  running");
-    next();
- }
+        //first middleware
+        //creation of middlewares
+        const  loggingMiddleware = function(req, res, next){
+            console.log("first middleware is  running");
+            next();
+        }
 
- //loading of middlewares
- app.use(loggingMiddleware);
+        //loading of middlewares
+        app.use(loggingMiddleware);
 
- //second middleware
- const authMiddleware =function(req,res,next){
-     console.log("second middleware is  running");
-     next();
- }
-  app.use(authMiddleware);
+        //second middleware
+        const authMiddleware =function(req,res,next){
+            console.log("second middleware is  running");
+            next();
+        }
+        app.use(authMiddleware);
 
- //third middle ware
+        //third middle ware
 
- const  validationMiddleware = function(req, res, next){
-    console.log("third middleware is  running");
-     //ending the Req res cycle
-    res.send("Execution ends here");
-} 
+        const  validationMiddleware = function(req, res, next){
+            console.log("third middleware is  running");
+            //ending the Req res cycle
+            res.send("Execution ends here");
+        } 
 
- app.use(validationMiddleware);
+        app.use(validationMiddleware);
+ */
+
+      const route = require('./routes/route');
+      //mounting the routes
+      app.use("/api" ,route);
+       
+      //-> /api/student
+      //-> /api/admin
  
 app.use(express.json());
+
 app.get("/",(req,res)=>{
     console.log(req.body);    
     res.send("Radhe-Radhe");
