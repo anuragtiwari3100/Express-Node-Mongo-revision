1ca2eae7792f3c62339689a6c1ea13f8bd4b18ed
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-23 09:17:47 +0530

    learn creation and use of middleware

--- Lec1/index.js
+++ Lec1/index.js
@@ -6,17 +6,39 @@ const port = 3000
 
 
 
+//middleware -logging , auth , validation
 
+//first middleware
+//creation of middlewares
+ const  loggingMiddleware = function(req, res, next){
+     console.log("first middleware is  running");
+     next();
+ }
 
+ //loading of middlewares
+ app.use(loggingMiddleware);
 
+ //second middleware
+ const authMiddleware =function(req,res,next){
+     console.log("second middleware is  running");
+     next();
+ }
+  app.use(authMiddleware);
 
-//loading middleware into the app
-//in built middleware
+ //third middle ware
+
+ const  validationMiddleware = function(req, res, next){
+    console.log("third middleware is  running");
+    next();
+ } 
+
+ app.use(validationMiddleware);
+ 
 app.use(express.json());
 app.get("/",(req,res)=>{
     console.log(req.body);    
     res.send("Radhe-Radhe");
-    console.log("Je to hostel bahar mil gaya hai...");
+    console.log("me hu route handler");
 })
 
 
