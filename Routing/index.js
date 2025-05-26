d954bcb305a74d24205e4acbd898355f72a3127d
anuragtiwari3100 <anuragtiwarigpj2005@gmail.con>
2025-05-26 11:13:07 +0530

    perform meathod chainning

--- Routing/index.js
+++ Routing/index.js
@@ -7,25 +7,21 @@ const port = 3000
 //     res.send("Radhe Krishna");
 // })
 
+  //chainning
 app.get("/",(req, res)=>{
         // res.send("Radhe Krishna")
    res.sendFile("./dummy.html",
     {root:__dirname})
    
-})
-
-app.post("/items",(req, res)=>{
+}).post("/items",(req, res)=>{
     res.json({x:1,y:2,z:3});
     res.send("Got a post request");
-})
-
-app.put("/items/:id",(req,res)=>{
+}).put("/items/:id",(req,res)=>{
     res.send("Get a put request");
-})
-
-app.delete("/items/id",(req,res)=>{
+}).delete("/items/id",(req,res)=>{
     res.send("got a delete request");
 })
+
 app.listen(port,()=>{
     console.log(`Our Application is runnig on the port no ${port}`)
 })
